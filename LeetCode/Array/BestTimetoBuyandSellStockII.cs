d3914f7740f52898d4a2505ec384af2f304ebecf
Gregory Levit <levitg@gmail.com>
2018-05-01 17:37:53 +0300

    Solution to Best Time to Buy and Sell Stock II

---
+++ LeetCode/Array/BestTimetoBuyandSellStockII.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    public class BestTimetoBuyandSellStockII
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int profit = 0;
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                int delta = prices[i + 1] - prices[i];
+                if (delta > 0)
+                {
+                    profit += delta;
+                }
+            }
+            return profit;
+
+        }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Array;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static int[] ConvertToArray(string str)
+        {
+            int[] arr =  str.Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+            return arr;
+        }
+
+        static void Main(string[] args)
+        {
+            int result = BestTimetoBuyandSellStockII.MaxProfit(ConvertToArray("7,1,5,3,6,4"));
+            Console.WriteLine($"result={result},expected=7");
+        }
+    }
+}
