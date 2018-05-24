a74d93b7495a76d1ea7e5aff87b1ad84875ffbe3
bin0m <levitg@gmail.com>
2018-05-24 18:47:19 +0300

    Palindrome Number(solution B)

--- LeetCode/Strings/PalindromeNumber.cs
+++ LeetCode/Strings/PalindromeNumber.cs
@@ -10,23 +10,42 @@ namespace LeetCode.Strings
     {
         public static bool IsPalindrome(int x)
         {
+            // edge cases, x is not palindrome when:
+            // - negative number
+            // - or last digit is 0 (but not when x==0 )
+            if (x < 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+            //Solution A: using string
+            //string str = x.ToString();
+            //int head = 0;
+            //int tail = str.Length - 1;
+            //while (head < tail)
+            //{
+            //    if (str[head] != str[tail])
+            //    {
+            //        return false;
+            //    }
+            //    head++;
+            //    tail--;
+            //}
+            //return true;
+
+            //Solution B: using Math
             if (x < 0)
             {
                 return false;
             }
-            string str = x.ToString();
-            int head = 0;
-            int tail = str.Length - 1;
-            while (head < tail)
+            int reverseNum = 0;
+            int n = x;
+            while (n > reverseNum)
             {
-                if (str[head] != str[tail])
-                {
-                    return false;
-                }
-                head++;
-                tail--;
+                int lastDigit = n % 10;
+                reverseNum = reverseNum * 10 + lastDigit;
+                n = (int)(n / 10);
             }
-            return true;
+            return n == reverseNum || n == reverseNum /10;            
         }
     }
 }
