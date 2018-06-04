67c4782581ffc13c9fce19e9b93e0b308503b189
bin0m <levitg@gmail.com>
2018-06-04 21:07:24 +0300

    PalindromeLinkedList, draft

---
+++ LeetCode/LinkedLists/PalindromeLinkedList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LinkedLists
+{
+    public class PalindromeLinkedList
+    {
+        public static bool IsPalindrome(ListNode head)
+        {
+            // 1.create reverse list by iterating through this list and create nodes from the tail
+
+            // 2. iterate through 2 lists and compare each node
+            return true;
+
+        }
+    }
+}
