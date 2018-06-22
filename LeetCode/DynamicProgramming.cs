3f88366ad87bd363690d5d7da0555cac2e203e15
bin0m <levitg@gmail.com>
2018-06-22 18:08:21 +0300

    add example input

--- LeetCode/DynamicProgramming.cs
+++ LeetCode/DynamicProgramming.cs
@@ -10,7 +10,13 @@ namespace LeetCode
     {
         public static int ClimbStairs(int n)
         {
-            ///
+            //Input: 4
+            // 1 + 1 + 1 + 1
+            // 1 + 2 + 1
+            // 1 + 1 + 2
+            // 2 + 1 + 1
+            // 2 + 2 
+            //Output: 5
             throw new NotImplementedException();
         }
     }
