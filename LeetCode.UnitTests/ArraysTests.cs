c237089a19d8f349a7af647370ed4f5a1b985019
bin0m <levitg@gmail.com>
2018-07-30 18:20:51 +0300

    Added Unit Tests Projects

---
+++ LeetCode.UnitTests/ArraysTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTests
+{
+    [TestClass]
+    public class ArraysTests
+    {
+        [TestMethod]
+        public void MaxProfitTest1()
+        {
+            Assert.AreEqual(Arrays.MaxProfit(new[] { 4 }), 0);
+        }
+
+        [TestMethod]
+        public void MaxProfitTest2()
+        {
+            Assert.AreEqual(Arrays.MaxProfit(new[] { 1, 2, 3, 4, 5 }), 4);
+        }
+
+        [TestMethod]
+        public void MaxProfitTest3()
+        {
+            Assert.AreEqual(Arrays.MaxProfit(new[] { 7, 1, 5, 3, 6, 4 }), 7);
+        }
+
+        [TestMethod]
+        public void MaxProfitTest4()
+        {
+            Assert.AreEqual(Arrays.MaxProfit(new[] { 7, 6, 4, 3, 1 }), 0);
+        }
+    }
+}
---
+++ LeetCode.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LeetCode.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCode.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4966e713-3424-4149-8676-4d813fbadeb5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
