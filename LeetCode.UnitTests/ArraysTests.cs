4c34c793e62fb43b6f1831754528c70440127bbf
bin0m <levitg@gmail.com>
2018-08-15 18:17:20 +0300

    fix the right order of expected and actual result in Unit Tests

--- LeetCode.UnitTests/ArraysTests.cs
+++ LeetCode.UnitTests/ArraysTests.cs
@@ -9,49 +9,49 @@ namespace LeetCode.UnitTests
         [TestMethod]
         public void MaxProfitTest1()
         {
-            Assert.AreEqual(Arrays.MaxProfit(new[] { 4 }), 0);
+            Assert.AreEqual(0, Arrays.MaxProfit(new[] { 4 }));
         }
 
         [TestMethod]
         public void MaxProfitTest2()
         {
-            Assert.AreEqual(Arrays.MaxProfit(new[] { 1, 2, 3, 4, 5 }), 4);
+            Assert.AreEqual(4, Arrays.MaxProfit(new[] { 1, 2, 3, 4, 5 }));
         }
 
         [TestMethod]
         public void MaxProfitTest3()
         {
-            Assert.AreEqual(Arrays.MaxProfit(new[] { 7, 1, 5, 3, 6, 4 }), 7);
+            Assert.AreEqual(7, Arrays.MaxProfit(new[] { 7, 1, 5, 3, 6, 4 }));
         }
 
         [TestMethod]
         public void MaxProfitTest4()
         {
-            Assert.AreEqual(Arrays.MaxProfit(new[] { 7, 6, 4, 3, 1 }), 0);
+            Assert.AreEqual(0, Arrays.MaxProfit(new[] { 7, 6, 4, 3, 1 }));
         }
 
         [TestMethod]
         public void ContainsDuplicateTest1()
         {
-            Assert.AreEqual(Arrays.ContainsDuplicate(new int[0]), false);
+            Assert.AreEqual(false, Arrays.ContainsDuplicate(new int[0]));
         }
 
         [TestMethod]
         public void ContainsDuplicateTest2()
         {
-            Assert.AreEqual(Arrays.ContainsDuplicate(new[] { 1 }), false);
+            Assert.AreEqual(false, Arrays.ContainsDuplicate(new[] { 1 }));
         }
 
         [TestMethod]
         public void ContainsDuplicateTest3()
         {
-            Assert.AreEqual(Arrays.ContainsDuplicate(new[] { 1, 2, 1 }), true);
+            Assert.AreEqual(true, Arrays.ContainsDuplicate(new[] { 1, 2, 1 }));
         }
 
         [TestMethod]
         public void ContainsDuplicateTest4()
         {
-            Assert.AreEqual(Arrays.ContainsDuplicate(new[] { 7, 6, 4, 3, 1 }), false);
+            Assert.AreEqual(false, Arrays.ContainsDuplicate(new[] { 7, 6, 4, 3, 1 }));
         }
     }
 }
